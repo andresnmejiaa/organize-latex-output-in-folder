1e14942fa52688a1c3c6a42c08dded01f3ca97fe
Andres-n-Mejia <50155815+Andres-n-Mejia@users.noreply.github.com>
2019-06-17 16:15:14 -0400

    Add files via upload
    grabs files in in home/Latex-Documents outputted by LaTex compiler. For example, if we have math.aux, math.pdf,math.tex, the script creates a new directory home/Latex-Documents/math and insider that directory it moves all three files math.aux, math.pdf,math.tex (although it will grab other files as well as long as they are of the form math.xxx.) I used this to organize my latex documents, so that there were no loose files flying around.

---
+++ organize.py
@@ -0,0 +1,36 @@
+##organize these laTex output.
+import os
+
+##enter the directory here
+path='/home/andres/Latex Documents/'
+myfiles=os.listdir(path)
+
+##write to a python dictionary of the form {mathdoc:[mathdoc.tex, mathdoc.pdf,...]} 
+##ignore it if there is no extension name to the file.
+
+def make_dict(directory,newfiles={}):
+    for file_ in directory:
+        temp=file_.split('.')
+        if len(temp)==1 or temp[1]=='py':
+            pass
+        else:
+            if temp[0] not in newfiles:
+                newfiles[temp[0]]=[file_]
+            else:
+                newfiles[temp[0]].append(file_)
+    return newfiles
+organized=make_dict(myfiles)
+##for each elem in a dictionary, make a directory called path+mathdoc, and move
+## .pdf,.tex garbage into this folder.
+def organize():
+    path='/home/andres/Latex Documents/'
+    for texname in organized:
+        name = path+texname
+        os.mkdir(name)
+        for texfile in organized[texname]:
+            filename = path+texfile
+            newname=name+'/'+texfile
+            os.rename(filename, newname)
+if __name__ == "__main__":
+    organize()
+            
